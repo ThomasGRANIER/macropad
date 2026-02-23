5ff987b79a9100fdac3ae8d36cf4b51cd1e51133
Thomas GRANIER <tgranier@seclab-security.com>
2026-02-23 14:45:45 +0100

    delete .sh file

--- macropad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ce place dans le bon dossier 
-cd /home/tgranier/macropad-python/
-
-# Définit le port par défaut
-PORT=/dev/ttyACM0
-
-# Lance le script Python
-python3 main.py -p "$PORT"
